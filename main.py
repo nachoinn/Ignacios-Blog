82415a0af4360364093282418028d12ab4a8082f
Ignacio <nw_nacho2@hotmail.com>
2022-07-27 23:06:45 -0300

    add psycopg2-binary, and freeze requirements, alternative database, and add changes for postgresql uri from heroku

--- main.py
+++ main.py
@@ -13,12 +13,17 @@ from forms import *
 from functools import wraps
 import bleach
 import os
+import re
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("Secret_Key")
 ckeditor = CKEditor(app)
 Bootstrap(app)
+
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL",  "sqlite:///blog.db")
